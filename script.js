913a55b3ab9874b80d96bfbcf259697604055a1c
andrijadragojevic <andrijadrago55@gmail.com>
2023-04-05 21:51:25 +0200

    Dodao atribute 'id' i 'isChecked'

--- script.js
+++ script.js
@@ -3,13 +3,14 @@ var tasks = [{}];
 
 function saveTask(){
     let task = {
+        "id": taskCount,
         "name": document.getElementById('taskName').value,
         "deadline": document.getElementById('taskDeadline').value,
-        "priority": document.getElementById('taskPriority').value
+        "priority": document.getElementById('taskPriority').value,
+        "isChecked": false
     }
 
     tasks.push(task);
-    taskCount++;
 
     let newRow = '<tr id="task'+taskCount+'">'
                         + '<td> <input type="checkbox" onchange="markTask('+taskCount+')"> </td>'
@@ -17,6 +18,8 @@ function saveTask(){
                         + '<td>'+ task.deadline +'</td>'
                         + '<td>'+ task.priority +'</td>'
                 + '</tr>';
+
+    taskCount++;
 
     document.getElementById('tasksTableBody').innerHTML += newRow;
     resetForm();
@@ -30,5 +33,10 @@ function resetForm(){
 }
 
 function markTask(cnt){
+    tasks.forEach(task => {
+        if (task.id == cnt) {
+            task.isChecked = !task.isChecked;
+        }
+    })
     document.getElementById('task'+cnt).classList.toggle('finished-task')
 }
