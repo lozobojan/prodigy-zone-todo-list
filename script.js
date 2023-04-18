252489df6690115ae5c9afc663b11b0c34d5ac24
Bojan Lozo <bojanlo@maestralsolutions.com>
2023-04-18 13:25:54 +0200

    Refaktor koda

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
-var taskCount = 0;
+var tasks = [
+    
+];
 
 function saveTask(){
     let taskName = document.getElementById('taskName').value;
     let taskDeadline = document.getElementById('taskDeadline').value;
     let taskPriority = document.getElementById('taskPriority').value;
 
-    taskCount++;
-    let newRow = '<tr id="task'+taskCount+'">'
-                        + '<td> <input type="checkbox" onchange="markTask('+taskCount+')"> </td>'
-                        + '<td>'+ taskName + '</td>'
-                        + '<td>'+taskDeadline+'</td>'
-                        + '<td>'+taskPriority+'</td>'
-                + '</tr>';
+    tasks.push({
+        name: taskName,
+        deadline: taskDeadline,
+        priority: taskPriority,
+        isDone: false
+    });
 
-    document.getElementById('tasksTableBody').innerHTML += newRow;
+    displayTable();
     resetForm();
 }
 
@@ -24,6 +25,41 @@ function resetForm(){
     });
 }
 
-function markTask(cnt){
-    document.getElementById('task'+cnt).classList.toggle('finished-task')
+function markTask(index){
+    tasks[index].isDone = !(tasks[index].isDone);
+    displayTable();
 }
+
+function attachListeners(){
+    document.querySelectorAll('.finish-chk').forEach(function(chk){
+        chk.addEventListener('change', function(e){
+            markTask(e.target.value);
+        });
+    });
+}
+
+function displayTable(){
+    let tableRows = "";
+    tasks.forEach( (task, index) => {
+        let checkedAttr = '';
+        let finishedClass = '';
+        if(task.isDone){
+            checkedAttr = 'checked';
+            finishedClass = 'finished-task';
+        };
+
+        let newRow = '<tr id="task'+index+'" class="'+finishedClass+'" >'
+                        + '<td> <input type="checkbox" '+checkedAttr+' class="finish-chk" value="'+index+'"> </td>'
+                        + '<td>'+ task.name + '</td>'
+                        + '<td>'+ task.deadline +'</td>'
+                        + '<td>'+ task.priority +'</td>'
+                + '</tr>';
+        tableRows += newRow;
+    });
+    document.getElementById("tasksTableBody").innerHTML = tableRows;
+    attachListeners();
+}
+
+document.getElementById('saveBtn').addEventListener('click', (e) => {
+    saveTask();
+});
